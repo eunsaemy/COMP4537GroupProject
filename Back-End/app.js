cc213d41a4a244512cf48c306d0fddf93ba2db02
Eunsaem Lee <eunsaemy@gmail.com>
2021-04-11 17:14:46 -0700

    CORS change

--- Back-End/app.js
+++ Back-End/app.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection({
 });
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://eunsaemlee.com');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-Width');
     next();
